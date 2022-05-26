0d563bcc37612fd11ad1e36c2881212adb73aad8
shivam256 <shivamgavandi256@gmail.com>
2022-05-26 12:54:30 +0530

    removed admin folder

--- client/src/pages/landing/landing.component.jsx
+++ client/src/pages/landing/landing.component.jsx
@@ -1,12 +1,27 @@
 import React from "react";
-import { Box } from "@mui/material";
+import {styled} from '@mui/material';
+import Vec1 from '../../assets/images/Vector 1.png';
+
+const BackgroundBox = styled("div")(({src})=>({
+  backgroundImage:`url('${src}')`,
+  width:'100%',
+  height:"100%",
+  backgroundPosition:'center',
+  backgroundSize:'cover'
+}))
 
 
 const LandingPage = () => {
   return (
     <div className="flex h-screen">
-      <div className="w-2/4 bg-red-500 h-full">image</div>
-      <div className="w-2/4 h-full ">welcome</div>
+      <div className="w-2/5 bg-red-500 h-full">image</div>
+      <div className="w-3/5 h-full ">
+        <BackgroundBox src={Vec1} >
+          <div>
+            hello
+          </div>
+        </BackgroundBox>
+      </div>
     </div>
   );
 };
