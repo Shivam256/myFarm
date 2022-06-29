10bdc53d6c26725f1a021deb5193dfaffed6b086
shivam256 <shivamgavandi256@gmail.com>
2022-06-29 11:30:58 +0530

    changes

--- client/src/pages/explore/explore.component.jsx
+++ client/src/pages/explore/explore.component.jsx
@@ -30,7 +30,7 @@ const Explore = () => {
               </div>
             </Link>
           </Grid>{" "}
-          <Grid item md={4}>
+          {/* <Grid item md={4}>
             <Link to="/client/cropRates">
               <div className="w-full h-60 shadow-lg rounded-lg overflow-hidden hover:shadow-xl group">
                 <div className="w-full h-full bg-[url('https://images.unsplash.com/photo-1579532536935-619928decd08?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')] bg-center bg-cover cursor-pointer hover:scale-125 transition-all ease-in-out">
@@ -40,7 +40,7 @@ const Explore = () => {
                 </div>
               </div>
             </Link>
-          </Grid>
+          </Grid> */}
         </Grid>
       </div>
     </div>
